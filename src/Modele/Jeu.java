20ba70691c77e2b48fca9f8e10ca314a8921f27e
Matteo <matteo.doyen@etu.univ-grenoble-alpes.fr>
2021-05-12 17:29:41 +0200

    feat (Jeu) : Ajout des fonctions concernant le déroulement du Jeu

--- src/Modele/Jeu.java
+++ src/Modele/Jeu.java
@@ -2,60 +2,132 @@ package Modele;
 
 import java.awt.*;
 import java.util.ArrayList;
-
+/**
+ *
+ */
 public class Jeu {
     Plateau plateau;
-
 
     public static final int JOUEUR1 = 16;
     public static final int JOUEUR2 = 32;
 
     private int joueur_en_cours;
+    private int situation;
+    private int nombre_batisseurs;
+    private Point batisseur_en_cours;
 
-    ArrayList<Point> ouvriersJoueur1;
-    ArrayList<Point> ouvriersJoueur2;
 
     /**
-     * Instantie une classe jeu
+     * Instantie une classe jeu.
+     * @param l
+     * @param c
      */
     public Jeu(int l, int c){
+        situation = 0;
         joueur_en_cours = JOUEUR1;
         plateau = new Plateau(l,c);
-        ouvriersJoueur1 = new ArrayList<>();
-        ouvriersJoueur2 = new ArrayList<>();
+        nombre_batisseurs = 0;
+        batisseur_en_cours = new Point();
     }
 
+    /**
+     *
+     * @param l un indice de ligne sur la grille
+     * @param c un indice de colonne sur la grille
+     * @return
+     * @see Plateau#estLibre(int ligne, int colonne)
+     * @see Plateau#ajouterJoueur(int ligne, int colonne, int type_joueur)
+     * @see #avancer(int ligne, int colonne, Point batisseur)
+     * @see #construire(int ligne, int colonne, Point batisseur)
+     */
+    public int jouer(int l, int c){
+        int retour = 0;
 
-    public void Jouer(int l,int c,Point ouvrier){
-        while(!Avancer(l, c, ouvrier)){
-            // LA CASE NEST PAS VALIDE
+        // placement des batisseurs sur la grille
+        if (nombre_batisseurs < 4) {
+            if(plateau.estLibre(l, c)) {
+                plateau.ajouterJoueur(l, c, joueur_en_cours);
+                nombre_batisseurs++;
+                if( nombre_batisseurs%2 == 0){
+                    finTour();
+                }
+            }
         }
-
-        joueur_en_cours = joueur_en_cours == JOUEUR1 ? JOUEUR2: JOUEUR1;
+        else if(situation == 0) {
+            batisseur_en_cours.x = l;
+            batisseur_en_cours.y = c;
+            situation = 1;
+            retour = situation;
+        }
+        else if(situation == 1){ // déplace un batisseur aux coordonées l et c de la grille
+            situation = avancer(l, c, batisseur_en_cours)? 2 : 1;
+            retour = situation;
+        }
+        else if(situation == 2){ // construit un bâtiment aux coordonées l et c de la grille si possib
+            if(construire(l,c,batisseur_en_cours)){
+                finTour();
+                situation = 0;
+                retour = 3;
+            }
+        }
+        return retour;
     }
 
-    public boolean Avancer(int l,int c,Point ouvrier){
-        if(plateau.atteignable(l,c,ouvrier.x,ouvrier.y) && plateau.estLibre(l,c) && plateau.deplacementPossible(l,c,ouvrier.x,ouvrier.y)){
-            plateau.ajouterJoueur(l,c,joueur_en_cours);
+    /**
+     * Fini le tour pour le joueur en cours.
+     */
+    public void finTour(){
+        joueur_en_cours = joueur_en_cours == JOUEUR1 ? JOUEUR2: JOUEUR1; // BOUTON "VALIDER"
+    }
+
+    /**
+     * Déplace le batisseur vers les coordonnées l et c si c'est possible.
+     * @param l un indice de ligne sur la grille
+     * @param c un indice de colonne sur la grille
+     * @param batisseur position (x;y) d'un batisseur
+     * @return vrai s'il a été possible de se déplacer
+     * @see Plateau#ajouterJoueur(int ligne, int colonne, int type_joueur)
+     * @see Plateau#atteignable(int ligne, int colonne, Point batisseur)
+     * @see Plateau#estLibre(int ligne, int colonne)
+     * @see Plateau#deplacementPossible(int ligne, int colonne, Point batisseur)
+     */
+    public boolean avancer(int l, int c, Point batisseur){
+        if(plateau.atteignable(l,c,batisseur) && plateau.estLibre(l,c) && plateau.deplacementPossible(l,c,batisseur)){
+            plateau.ajouterJoueur(batisseur.x,batisseur.y,0); // enlève le batisseurs de la case du point batisseurs
+            plateau.ajouterJoueur(l,c,joueur_en_cours); // ajoute un batisseurs à la case en position l,c
             return true;
         }
         return false;
     }
-//
-//    public boolean Construire(){
-//
-//    }
 
     /**
-     * peutConstruire
-     * Vérifie si un @ouvrier peut construire sur la case en @l @c
+     * Construit aux coordonnées l et c une hauteur de bâtiment si c'est possible.
      * @param l un indice de ligne sur la grille
      * @param c un indice de colonne sur la grille
-     * @param ouvrier le Point d'un ouvrier
-     * @see Plateau#atteignable(int, int, int, int)
-     * @return vrai si l'ouvrier peut construire ici.
+     * @param batisseur position (x;y) d'un batisseur
+     * @return vrai si la construction a bien eu lieu
+     * @see #peutConstruire(int ligne, int colonne, Point batisseur)
+     * @see Plateau#ameliorerBatiment(int ligne, int colonne)
      */
-    public boolean peutConstruire(int l, int c,Point ouvrier){
-        return (plateau.atteignable(ouvrier.x,ouvrier.y,l,c) && !plateau.estCoupole(l,c) && plateau.estLibre(l,c));
+    private boolean construire(int l, int c, Point batisseur){
+        if(peutConstruire(l, c, batisseur)) {
+            return plateau.ameliorerBatiment(l, c);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Vérifie si un ouvrier peut construire sur la case de la grille en l et c
+     * @param l un indice de ligne sur la grille
+     * @param c un indice de colonne sur la grille
+     * @param batisseur position (x;y) d'un batisseur
+     * @return vrai si le batisseur peut construire ici.
+     * @see Plateau#atteignable(int ligne, int colonne, Point batisseur)
+     * @see Plateau#estCoupole(int ligne, int colonne)
+     * @see Plateau#estLibre(int ligne, int colonne)
+     */
+    public boolean peutConstruire(int l, int c,Point batisseur){
+        return (plateau.atteignable(l,c,batisseur) && !plateau.estCoupole(l,c) && plateau.estLibre(l,c));
     }
 }
--- src/Modele/Plateau.java
+++ src/Modele/Plateau.java
@@ -1,5 +1,10 @@
 package Modele;
 
+import java.awt.*;
+
+/**
+ *
+ */
 public class Plateau {
     private int[][] cases;
     private int lignes;
@@ -23,17 +28,17 @@ public class Plateau {
     }
 
     /**
-     * estVide
+     *
      * @param l un indice de ligne sur la grille
      * @param c un indice de colonne sur la grille
      * @return vrai si la case de la grille est vide
+     *
      */
     public boolean estVide(int l, int c) {
         return (cases[l][c] & 0xF) == VIDE;
     }
 
     /**
-     * estRDC
      * @param l un indice de ligne sur la grille
      * @param c un indice de colonne sur la grille
      * @return vrai si la case de la grille est un rez-de-chaussée (bâtiment de hauteur 1)
@@ -43,7 +48,6 @@ public class Plateau {
     }
 
     /**
-     * estEtage
      * @param l un indice de ligne sur la grille
      * @param c un indice de colonne sur la grille
      * @return vrai si la case de la grille est un étage (bâtiment de hauteur 2)
@@ -53,89 +57,82 @@ public class Plateau {
     }
 
     /**
-     * estToit
      * @param l un indice de ligne sur la grille
      * @param c un indice de colonne sur la grille
-     * @return vrai si la case de la grille est un toit (bâtiment de hauteur 3)
+     * @return boolean si la case de la grille est un toit (bâtiment de hauteur 3)
      */
     public boolean estToit(int l, int c) {
         return (cases[l][c] & 0xF) == TOIT;
     }
 
     /**
-     * estCoupole
      * @param l un indice de ligne sur la grille
      * @param c un indice de colonne sur la grille
-     * @return vrai si la case de la grille est une coupole (bâtiment de hauteur 4)
+     * @return boolean si la case de la grille est une coupole (bâtiment de hauteur 4)
      */
     public boolean estCoupole(int l, int c) {
         return (cases[l][c] & 0xF) == COUPOLE;
     }
 
     /**
-     * ajouterRDC
      * Ajoute un rez-de-chaussée (bâtiment de hauteur 1) sur une case de la grille
-     *
      * @param l un indice de ligne sur la grille
      * @param c un indice de colonne sur la grille
-     * @return
      */
     public void ajouterRDC(int l, int c) {
         cases[l][c] = (cases[l][c] & ~0xf) | RDC;
     }
 
     /**
-     * ajouterEtage
      * Ajoute un étage (bâtiment de hauteur 2) sur une case de la grille
-     *
      * @param l un indice de ligne sur la grille
      * @param c un indice de colonne sur la grille
-     * @return
      */
     public void ajouterEtage(int l, int c) {
         cases[l][c] = (cases[l][c] & ~0xf) | ETAGE;
     }
 
     /**
-     * ajouterToit
      * Ajoute un toit (bâtiment de hauteur 3) sur une case de la grille
-     *
      * @param l un indice de ligne sur la grille
      * @param c un indice de colonne sur la grille
-     * @return
      */
     public void ajouterToit(int l, int c) {
         cases[l][c] = (cases[l][c] & ~0xf) | TOIT;
     }
 
     /**
-     * ajouterCoupole
      * Ajoute une coupole (bâtiment de hauteur 4) sur une case de la grille
-     *
      * @param l un indice de ligne sur la grille
      * @param c un indice de colonne sur la grille
-     * @return
      */
     public void ajouterCoupole(int l, int c) {
         cases[l][c] = (cases[l][c] & ~0xf) | COUPOLE;
     }
 
     /**
-     * estJoueur
-     * Vérifie que l'on peut sélectionner l'ouvrier selectionné appartient au joueur
+     * Ameliore le batiment à la position l c si c'est possible, considère qu'il n'y pas de batisseurs dessus
+     * @param l un indice de ligne sur la grille
+     * @param c un indice de colonne sur la grille
+     * @return vrai si l'amélioration a marché
+     */
+    public boolean ameliorerBatiment(int l, int c){
+        cases[l][c] = cases[l][c] << 1;
+        return true;
+    }
+
+    /**
+     * Vérifie que l'on peut sélectionner le batisseur selectionné appartient au joueur
      * @param l un indice de ligne sur la grille
      * @param c un indice de colonne sur la grille
      * @param joueur
-     * @return
      */
-    public boolean estJoueur(int l,int c, int joueur){
+    public boolean estBatisseur(int l,int c, int joueur){
         return (cases[l][c] & ~0xf) == joueur;
     }
 
     /**
-     * estJoueur
      * Vérifie que la case de la grille ne contient pas de joueur
-     *
      * @param l un indice de ligne sur la grille
      * @param c un indice de colonne sur la grille
      * @return
@@ -145,36 +142,38 @@ public class Plateau {
     }
 
     /**
-     * deplacementPossible
-     * Vérifie que la case de la grille
-     *
+     * Vérifie que la case de la grille visé peut acceuillir le batisseurs (deplacement vers le haut de uniquement 1 de hauteur)
      * @param l un indice de ligne sur la grille
      * @param c un indice de colonne sur la grille
-     * @param dl
-     * @param dc
+     * @param batisseur position (x;y) d'un batisseur
      * @return
      */
-
-    public boolean deplacementPossible(int l, int c, int dl, int dc){
-        int temp = (cases[dl][dc]&0xf) >> 1;
+    public boolean deplacementPossible(int l, int c, Point batisseur){
+        int temp = (cases[batisseur.x][batisseur.y]&0xf) >> 1;
         return temp <= (cases[l][c] &0xf);
     }
 
+    /**
+     * Ajoute un joueur sur la case de la grille.
+     * @param l un indice de ligne sur la grille
+     * @param c un indice de colonne sur la grille
+     * @param joueur
+     */
     public void ajouterJoueur(int l, int c, int joueur){
-        cases[l][c] = cases[l][c] & (~0xf) | joueur;
+        cases[l][c] = ((cases[l][c] & 0xf) | joueur);
     }
 
     /**
-     * atteignable
      *
      * @param l un indice de ligne sur la grille
      * @param c un indice de colonne sur la grille
+     * @param batisseur position (x;y) d'un batisseur
      * @return
      */
-    public boolean atteignable(int l, int c, int dl, int dc){
-        int a = Math.abs(l-dl);
-        int b = Math.abs(c-dc);
-        return a+b>0 && a<2 && b<2;
+    public boolean atteignable(int l, int c, Point batisseur){
+        int a = Math.abs(l-batisseur.x);
+        int b = Math.abs(c-batisseur.y);
+        return (a + b > 0) && (a < 2) && (b < 2);
     }
 
     public int getLignes() {
