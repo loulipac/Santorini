533c7225c1e0002374d20c5e11035e006e06f3d5
Benoit PEGAZ <benoitpegaz@gmail.com>
2021-05-13 10:06:04 +0200

    fix(IHM) : Correction bouton Quitter

--- src/Vue/PanelMenu.java
+++ src/Vue/PanelMenu.java
@@ -2,19 +2,19 @@ package Vue;
 
 import Modele.BoutonMenu;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-/**
- * @TODO Reussir a afficher bQuitter en tant que BoutonMenu et non JButton
- */
+
 class PanelMenu extends JPanel {
 
-//    private JButton bQuitter;
-    private BoutonMenu bQuitter;
-    private BoutonMenu bJouer, bTutoriel, bRegles;
+    private BoutonMenu bJouer, bTutoriel, bRegles, bQuitter;
     private JLabel titre;
 
     public PanelMenu() {
@@ -30,16 +30,7 @@ class PanelMenu extends JPanel {
         bJouer = new BoutonMenu("src/Ressources/bouton_menu/jouer.png", 415, 90);
         bTutoriel = new BoutonMenu("src/Ressources/bouton_menu/tutoriel.png", 415, 90);
         bRegles = new BoutonMenu("src/Ressources/bouton_menu/regle_jeu.png", 415, 90);
-        bQuitter = new BoutonMenu("src/Ressources/bouton_menu/barre_vide.png", 415, 90);
-
-//        bQuitter = new JButton(new ImageIcon("src/Ressources/assets_recurrents/barre_vide.png"));
-//        bQuitter.setOpaque(false);
-//        bQuitter.setFocusPainted(false);
-//        bQuitter.setBorderPainted(false);
-//        bQuitter.setContentAreaFilled(false);
-//        bQuitter.setBorder(null);
-//        bQuitter.setAlignmentX(CENTER_ALIGNMENT);
-//        bQuitter.setMaximumSize(new Dimension(415, 90));
+        bQuitter = new BoutonMenu("src/Ressources/bouton_menu/quitter.png", 415, 90);
 
         /* Label */
 
@@ -78,16 +69,15 @@ class PanelMenu extends JPanel {
         f.getCardLayout().show(f.mainPanel, "plateau");
     }
 
-    /*public void paintComponent(Graphics g)
-    {
-        //Chargement de l"image de fond
-        try {
-            BufferedImage img = ImageIO.read(new File("src/Ressources/artwork/base.png"));
-            g.drawImage(img, this.getWidth()/5, this.getHeight()/5, 900, 540, this);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("Erreur image de fond: " +e.getMessage());
-        }
-    }*/
+//    public void paintComponent(Graphics g)
+//    {
+//        //Chargement de l"image de fond
+//        try {
+//            BufferedImage img = ImageIO.read(new File("src/Ressources/artwork/base.png"));
+//            g.drawImage(img, this.getWidth()/5, this.getHeight()/5, 900, 540, this);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            System.out.println("Erreur image de fond: " +e.getMessage());
+//        }
+//    }
 }
