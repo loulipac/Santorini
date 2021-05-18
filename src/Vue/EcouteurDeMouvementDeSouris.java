462e4b152e3680328bca992a3960b4a867024e7d
Yanis <yanis.miollany@gmail.com>
2021-05-18 15:50:35 +0200

    feat(IHM Curseur) : Ajout d'un curseur gris quand inatteignable

--- src/Vue/EcouteurDeMouvementDeSouris.java
+++ src/Vue/EcouteurDeMouvementDeSouris.java
@@ -14,8 +14,10 @@ public class EcouteurDeMouvementDeSouris implements MouseMotionListener {
     int largeur_plateau, hauteur_plateau;
     Cursor c_construire_rouge;
     Cursor c_construire_bleu;
+    Cursor c_construire_gris;
     Cursor c_deplacer_rouge;
     Cursor c_deplacer_bleu;
+    Cursor c_deplacer_gris;
 
     public EcouteurDeMouvementDeSouris(Jeu j, JeuGraphique jg) {
         this.jg = jg;
@@ -28,13 +30,17 @@ public class EcouteurDeMouvementDeSouris implements MouseMotionListener {
 
         Image image_pas_bleu = toolkit.getImage(Constante.CHEMIN_RESSOURCE + "/curseur/pas_bleu.png");
         Image image_pas_rouge = toolkit.getImage(Constante.CHEMIN_RESSOURCE + "/curseur/pas_rouge.png");
+        Image image_pas_gris = toolkit.getImage(Constante.CHEMIN_RESSOURCE + "/curseur/pas_gris.png");
         Image image_outil_bleu = toolkit.getImage(Constante.CHEMIN_RESSOURCE + "/curseur/outil_bleu.png");
         Image image_outil_rouge = toolkit.getImage(Constante.CHEMIN_RESSOURCE + "/curseur/outil_rouge.png");
+        Image image_outil_gris = toolkit.getImage(Constante.CHEMIN_RESSOURCE + "/curseur/outil_gris.png");
         Point p = new Point(16, 16);
         c_deplacer_bleu = toolkit.createCustomCursor(image_pas_bleu, p, "c_pas_bleu");
         c_deplacer_rouge = toolkit.createCustomCursor(image_pas_rouge, p, "c_pas_rouge");
+        c_deplacer_gris = toolkit.createCustomCursor(image_pas_gris, p, "c_deplacer_gris");
         c_construire_bleu = toolkit.createCustomCursor(image_outil_bleu, p, "c_outil_bleu");
         c_construire_rouge = toolkit.createCustomCursor(image_outil_rouge, p, "c_outil_rouge");
+        c_construire_gris = toolkit.createCustomCursor(image_outil_gris, p, "c_construire_gris");
     }
 
     @Override
@@ -50,17 +56,26 @@ public class EcouteurDeMouvementDeSouris implements MouseMotionListener {
             int pos_x = e.getX() / jg.getTailleCase();
             int pos_y = e.getY() / jg.getTailleCase();
             if (j.getSituation() == Constante.DEPLACEMENT) {
-                if (j.getJoueur_en_cours() == Constante.JOUEUR1) {
-                    jg.setCursor(c_deplacer_bleu);
+                if(jg.getJeu().estAtteignable(pos_y, pos_x)) {
+                    if (j.getJoueur_en_cours() == Constante.JOUEUR1) {
+                        jg.setCursor(c_deplacer_bleu);
+                    } else {
+                        jg.setCursor(c_deplacer_rouge);
+                    }
                 } else {
-                    jg.setCursor(c_deplacer_rouge);
+                    jg.setCursor(c_deplacer_gris);
                 }
             } else if (j.getSituation() == Constante.CONSTRUCTION) {
-                if (j.getJoueur_en_cours() == Constante.JOUEUR1) {
-                    jg.setCursor(c_construire_bleu);
+                if(jg.getJeu().estAtteignable(pos_y, pos_x)) {
+                    if (j.getJoueur_en_cours() == Constante.JOUEUR1) {
+                        jg.setCursor(c_construire_bleu);
+                    } else {
+                        jg.setCursor(c_construire_rouge);
+                    }
                 } else {
-                    jg.setCursor(c_construire_rouge);
+                    jg.setCursor(c_construire_gris);
                 }
+
             } else {
                 jg.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
             }
