c4c0b1780ec0c4d1f2e213e93a4f3f6e1c914a73
Yanis <yanis.miollany@gmail.com>
2021-05-19 18:10:24 +0200

    docs(Constante)

--- src/Modele/Constante.java
+++ src/Modele/Constante.java
@@ -1,13 +1,19 @@
 package Modele;
 
+/**
+ * Fichier de constante.
+ */
 public class Constante {
+
+    /**
+     * Chemin vers le dossier de ressource ({@value #CHEMIN_RESSOURCE})
+     */
     public static final String CHEMIN_RESSOURCE = "./src/Ressources";
 
     public static final int SELECTION = 0;
     public static final int DEPLACEMENT = 1;
     public static final int CONSTRUCTION = 2;
     public static final int PLACEMENT = 3;
-
     public static final int JOUEUR1 = 8;
     public static final int JOUEUR2 = 16;
 }
