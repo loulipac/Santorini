f09fdf8e1e3296096e2b42da79cec1b7ecc92f75
Yanis <yanis.miollany@gmail.com>
2021-05-20 12:00:27 +0200

    feat(Constante) : Ajout de constante pour la taille de fenêtre

--- src/Modele/Constante.java
+++ src/Modele/Constante.java
@@ -12,7 +12,11 @@ public class Constante {
      */
     public static final String CHEMIN_RESSOURCE = "./src/Ressources";
 
-    public static final Dimension DEFAULT_FENETRE_TAILLE = new Dimension(1600, 900);
+    public static final Dimension FENETRE_1920x1080 = new Dimension(1920, 1080);
+    public static final Dimension FENETRE_1600x1900 = new Dimension(1600, 900);
+    public static final Dimension FENETRE_1366x768 = new Dimension(1366, 768);
+    public static final Dimension FENETRE_1024x768 = new Dimension(1024, 768);
+    public static final Dimension DEFAULT_FENETRE_TAILLE = FENETRE_1600x1900;
 
     public static final int SELECTION = 0;
     public static final int DEPLACEMENT = 1;
