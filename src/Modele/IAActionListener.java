44e6e31c044d85d7b0efc9b78323915d1c1652ec
Yanis <yanis.miollany@gmail.com>
2021-05-24 10:13:02 +0200

    fix : Désactiver pour le moment

--- src/Modele/IAActionListener.java
+++ src/Modele/IAActionListener.java
@@ -7,18 +7,15 @@ import java.awt.event.ActionListener;
 import static Modele.Constante.PLACEMENT;
 
 public class IAActionListener implements ActionListener {
-
     Timer timer;
-    IA ia;
-
-    public IAActionListener(IA ia) {
-        this.ia = ia;
-        timer = new Timer(1, this);
+    Jeu jeu;
+    public IAActionListener(Jeu _jeu) {
+        jeu = _jeu;
+        timer = new Timer(800, this);
         timer.start();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(ia.joue() == PLACEMENT) ia.joue();
     }
 }
