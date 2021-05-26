0e1667a24382316d8bba1c3fec0129c1e46a746f
Yanis <yanis.miollany@gmail.com>
2021-05-26 11:33:47 +0200

    fix (IA) : random inutile

--- src/Modele/IAFacile.java
+++ src/Modele/IAFacile.java
@@ -40,10 +40,7 @@ public class IAFacile implements IA {
         Point batisseur = batisseurs.get(index_batisseur);
         System.out.println("Batisseur à la position (" + batisseur.x + ", " + batisseur.y + ")");
         ArrayList<Point> accessibles = j.getPlateau().getCasesAccessibles(batisseur);
-        Point case_random;
-        do {
-            case_random = accessibles.get(random.nextInt(accessibles.size()));
-        } while (!j.getPlateau().deplacementPossible(case_random, batisseur));
+        Point case_random = accessibles.get(random.nextInt(accessibles.size()));
         batisseurs.set(index_batisseur, case_random);
         System.out.println("Envoie position déplacement. Est libre ?" + j.getPlateau().estLibre(case_random));
         //System.out.println("Batisseur déplacé : (" + case_random.x + ", " + case_random.y + ")");
@@ -56,10 +53,7 @@ public class IAFacile implements IA {
         System.out.println("Batisseur à la position (" + batisseur.x + ", " + batisseur.y + ")");
 
         ArrayList<Point> construction_possible = j.getPlateau().getConstructionsPossible(batisseur);
-        Point case_random;
-        do {
-            case_random = construction_possible.get(random.nextInt(construction_possible.size()));
-        } while (!j.getPlateau().peutConstruire(case_random, batisseur));
+        Point  case_random = construction_possible.get(random.nextInt(construction_possible.size()));
         System.out.println("Envoie position construction. Est libre ?" + j.getPlateau().estLibre(case_random));
         return case_random;
     }
