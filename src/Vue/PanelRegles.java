0c5311e94767986e98b34f5e4215fc9ff46fd403
Yanis <yanis.miollany@gmail.com>
2021-05-30 14:12:52 +0200

    refactor : Changement sur panel règles

--- src/Vue/PanelRegles.java
+++ src/Vue/PanelRegles.java
@@ -11,21 +11,24 @@ import java.io.File;
 import java.io.IOException;
 
 public class PanelRegles extends JPanel {
-    final static String TITRE_SECTION1 = "Comment jouer ?";
-    final static String TITRE_SECTION2 = "Déplacement";
-    final static String TITRE_SECTION3 = "Construction";
-    final static String CONTENU_SECTION1 = "Ce jeu se joue à 2 joueurs au tour par tour.\n" +
-            "Le but du jeu est de monter le plus vite possible au sommet \n" +
-            "d'une tour de 3 étages qu'il faut construire.\n" +
-            "Chaque joueur dispose de 2 bâtisseur qu'il place sur le plateau au début de la partie.\n" +
-            "A chaque tour, le joueur doit sélectionner un bâtisseur à déplacer\n" +
-            "et construire un étage dans les cases adjacentes.";
-    final static String CONTENU_SECTION2 = "Le bâtisseur choisi peut se déplacer sur un des emplacements proposés. \n" +
-            "Le bâtisseur ne peut monter que d'un étage à la fois et ne peut pas se\n" +
-            "déplacer sur un dôme.";
-    final static String CONTENU_SECTION3 = "Un bâtisseur peut construire un étage sur les emplacements proposés.\n" +
-            "Le bâtisseur peut poser un dôme en haut de la tour pour bloquer son adversaire. \n" +
-            "On considère une tour de 3 étages et un dôme comme une \"Tour Complète\".";
+    private static final String TITRE_SECTION1 = "Comment jouer ?";
+    private static final String TITRE_SECTION2 = "Déplacement";
+    private static final String TITRE_SECTION3 = "Construction";
+    private static final String CONTENU_SECTION1 = """
+            Ce jeu se joue à 2 joueurs au tour par tour.
+            Le but du jeu est de monter le plus vite possible au sommet
+            d'une tour de 3 étages qu'il faut construire.
+            Chaque joueur dispose de 2 bâtisseur qu'il place sur le plateau au début de la partie.
+            A chaque tour, le joueur doit sélectionner un bâtisseur à déplacer
+            et construire un étage dans les cases adjacentes.""";
+    private static final String CONTENU_SECTION2 = """
+            Le bâtisseur choisi peut se déplacer sur un des emplacements proposés.
+            Le bâtisseur ne peut monter que d'un étage à la fois et ne peut pas se
+            déplacer sur un dôme.""";
+    private static final String CONTENU_SECTION3 = """
+            Un bâtisseur peut construire un étage sur les emplacements proposés.
+            Le bâtisseur peut poser un dôme en haut de la tour pour bloquer son adversaire.
+            On considère une tour de 3 étages et un dôme comme une "Tour Complète".""";
 
     Font lilly_belle;
     Dimension taille_fenetre;
@@ -38,7 +41,7 @@ public class PanelRegles extends JPanel {
         } catch (IOException | FontFormatException e) {
             System.err.println("Erreur : La police 'LilyScriptOne' est introuvable ");
         }
-        lilly_belle = new Font("Lily Script One", Font.TRUETYPE_FONT, 26);
+        lilly_belle = new Font("Lily Script One", Font.PLAIN, 26);
         taille_fenetre = _taille_fenetre;
         initialiserPanel();
     }
@@ -50,84 +53,123 @@ public class PanelRegles extends JPanel {
         setBackground(new Color(47, 112, 162));
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-        /* Label */
-        JLabel logo = new JLabel(new ImageIcon(CHEMIN_RESSOURCE + "/logo/logo.png"));
-        logo.setAlignmentX(CENTER_ALIGNMENT);
-        logo.setMaximumSize(new Dimension(415, 100));
-
         /* Boutons */
-        Bouton bRetour = new Bouton(CHEMIN_RESSOURCE + "/bouton/retour.png", CHEMIN_RESSOURCE + "/bouton/retour_hover.png", taille_fenetre.width / 4, taille_fenetre.height / 20, this::actionBoutonRetourMenu);
 
         /* Panel */
-        ReglesPanel panel = new ReglesPanel();
+        Dimension taille_panel = new Dimension((int) (taille_fenetre.width * 0.6), (int) (taille_fenetre.height * 0.8));
+        ReglesPanel panel = new ReglesPanel(taille_panel);
 
         /* Adding */
-        add(Box.createRigidArea(new Dimension(taille_fenetre.width, taille_fenetre.height / 20)));
-        add(logo);
+        addMargin(this, new Dimension(taille_fenetre.width, (int) (taille_fenetre.height * 0.10)));
         add(panel);
-        add(Box.createRigidArea(new Dimension(taille_fenetre.width, taille_fenetre.height / 40)));
-        add(bRetour);
         setVisible(true);
     }
 
+    private void addMargin(JPanel parent, Dimension taille) {
+        parent.add(Box.createRigidArea(taille));
+    }
+
     private class LignePanel extends JPanel {
-        public LignePanel(Dimension taille_panel, String image_path, JLabel titre, JTextArea texte) {
+        public LignePanel(Dimension taille_panel, String image_path, String titre, String texte) {
             setOpaque(false);
             setMaximumSize(taille_panel);
+            setLayout(new BorderLayout());
 
-            /* Images */
-            JLabel image = creerImage(image_path, taille_panel.width, taille_panel.height);
-
-            /* Panels*/
             JPanel panel_image = new JPanel();
-            JPanel panel_texte = new JPanel();
-
+            panel_image.setLayout(new GridBagLayout());
             panel_image.setOpaque(false);
-            panel_image.setPreferredSize(new Dimension(taille_panel.width, taille_panel.height));
+            Dimension taille_panel_image = new Dimension(taille_panel.width / 4, taille_panel.height);
+            panel_image.setPreferredSize(taille_panel_image);
+            panel_image.setMaximumSize(taille_panel_image);
+            panel_image.add(creerImage(image_path, taille_panel_image));
 
-            panel_texte.setOpaque(false);
-            panel_texte.setPreferredSize(new Dimension(taille_panel.width * 2, taille_panel.height));
+            JPanel contenu = new JPanel();
+            contenu.setLayout(new BorderLayout());
+            contenu.setOpaque(false);
+            Dimension taille_panel_contenu = new Dimension(taille_panel.width - taille_panel.width / 4, taille_panel.height);
+            contenu.setMaximumSize(taille_panel_contenu);
+            contenu.setPreferredSize(taille_panel_contenu);
+            contenu.add(creerTitre(titre), BorderLayout.NORTH);
+            contenu.add(creerContenu(texte), BorderLayout.CENTER);
 
-            texte.setPreferredSize(new Dimension(taille_panel.width * 2, taille_panel.height));
+            add(panel_image, BorderLayout.WEST);
+            add(contenu, BorderLayout.EAST);
+        }
 
-            /* Adding */
-            panel_image.add(image);
-            panel_texte.add(titre);
-            panel_texte.add(Box.createRigidArea(new Dimension(taille_panel.width * 2, 0)));
-            panel_texte.add(texte);
-            add(panel_image);
-            add(panel_texte);
+        private JLabel creerTitre(String titre) {
+            JLabel _e = new JLabel(titre);
+            _e.setFont(lilly_belle);
+            _e.setForeground(new Color(82, 60, 43));
+            return _e;
+        }
+
+        private JTextArea creerContenu(String contenu) {
+            JTextArea _e = new JTextArea(contenu);
+            _e.setOpaque(false);
+            _e.setEditable(false);
+            _e.setFont(new Font("Arial", Font.PLAIN, 14));
+            return _e;
         }
     }
 
     private class ReglesPanel extends JPanel {
-        public ReglesPanel() {
+        private final Dimension taille_panel;
+
+        public ReglesPanel(Dimension taille_panel) {
+            this.taille_panel = taille_panel;
+
+            setMaximumSize(taille_panel);
+            setPreferredSize(taille_panel);
             setOpaque(false);
+            setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-            JLabel titreCommentJouer = creerTitre(TITRE_SECTION1);
-            JLabel titreDeplacement = creerTitre(TITRE_SECTION2);
-            JLabel titreConstruction = creerTitre(TITRE_SECTION3);
 
-            /* TextArea */
-            JTextArea texteCommentJouer = creerContenu(CONTENU_SECTION1);
-            JTextArea texteDeplacement = creerContenu(CONTENU_SECTION2);
-            JTextArea texteConstruction = creerContenu(CONTENU_SECTION3);
+            int largeur_sous_panel = (int) Math.round(taille_panel.width * 0.9);
 
-            int largeur_panel = taille_fenetre.width * 5 / 8;
-            int hauteur_panel = taille_fenetre.height * 5 / 8;
-            int largeur_sous_panel = (int) Math.round(largeur_panel * 0.9);
-            int hauteur_sous_panel = (int) Math.round(hauteur_panel / 3.5);
-            Dimension taille_sous_panel = new Dimension(largeur_sous_panel / 3, hauteur_sous_panel);
-            setMaximumSize(new Dimension(largeur_panel, hauteur_panel));
+            double ratio_marge = 0.02;
+            double ratio_titre = 0.15;
+            double ratio_bouton = 0.10;
+            double ratio_taille = 1 - ratio_marge * 3 - ratio_titre - ratio_bouton;
+            int hauteur_sous_panel = (int) (taille_panel.height * (ratio_taille / 3));
 
-            LignePanel sous_panel_1 = new LignePanel(taille_sous_panel, CHEMIN_RESSOURCE + "/artwork/comment_jouer.png", titreCommentJouer, texteCommentJouer);
-            LignePanel sous_panel_2 = new LignePanel(taille_sous_panel, CHEMIN_RESSOURCE + "/artwork/deplacement.png", titreDeplacement, texteDeplacement);
-            LignePanel sous_panel_3 = new LignePanel(taille_sous_panel, CHEMIN_RESSOURCE + "/artwork/construction.png", titreConstruction, texteConstruction);
 
-            add(Box.createRigidArea(new Dimension(largeur_sous_panel, hauteur_sous_panel / 10)));
+            JPanel panel_titre = new JPanel();
+            JLabel titre = new JLabel("Règles");
+            titre.setFont(new Font("Lily Script One", Font.PLAIN, 45));
+            panel_titre.setLayout(new GridBagLayout());
+            panel_titre.setOpaque(false);
+            panel_titre.setMaximumSize(new Dimension(taille_panel.width, (int) (taille_panel.height * ratio_titre)));
+            panel_titre.setPreferredSize(new Dimension(taille_panel.width, (int) (taille_panel.height * ratio_titre)));
+            panel_titre.add(titre);
+
+            Dimension taille_sous_panel = new Dimension(largeur_sous_panel, hauteur_sous_panel);
+
+            LignePanel sous_panel_1 = new LignePanel(
+                    taille_sous_panel, CHEMIN_RESSOURCE + "/artwork/comment_jouer.png",
+                    TITRE_SECTION1, CONTENU_SECTION1);
+            LignePanel sous_panel_2 = new LignePanel(
+                    taille_sous_panel, CHEMIN_RESSOURCE + "/artwork/deplacement.png",
+                    TITRE_SECTION2, CONTENU_SECTION2);
+            LignePanel sous_panel_3 = new LignePanel(
+                    taille_sous_panel, CHEMIN_RESSOURCE + "/artwork/construction.png",
+                    TITRE_SECTION3, CONTENU_SECTION3);
+
+
+            double height_bouton = taille_panel.height * ratio_bouton;
+            Dimension taille_bouton = new Dimension(
+                    (int) (height_bouton * RATIO_BOUTON_CLASSIQUE),
+                    (int) (height_bouton)
+            );
+            Bouton bRetour = new Bouton(CHEMIN_RESSOURCE + "/bouton/retour.png", CHEMIN_RESSOURCE + "/bouton/retour_hover.png",
+                    taille_bouton.width,
+                    taille_bouton.height,
+                    PanelRegles.this::actionBoutonRetourMenu);
+
+            add(panel_titre);
             add(sous_panel_1);
             add(sous_panel_2);
             add(sous_panel_3);
+            add(bRetour);
         }
 
         @Override
@@ -143,8 +185,8 @@ public class PanelRegles extends JPanel {
                         bg_regles,
                         0,
                         0,
-                        getSize().width,
-                        getSize().height,
+                        taille_panel.width,
+                        taille_panel.height,
                         this
                 );
             } catch (Exception e) {
@@ -159,29 +201,15 @@ public class PanelRegles extends JPanel {
         f.displayPanel("menu");
     }
 
-    private JLabel creerTitre(String titre) {
-        JLabel _e = new JLabel(titre);
-        _e.setFont(lilly_belle);
-        _e.setForeground(new Color(82, 60, 43));
-        return _e;
+    public JLabel creerImage(String _in, Dimension _t) {
+        ImageIcon _imIcon = new ImageIcon(_in);
+        Image _im = _imIcon.getImage();
+        Dimension _nt = conserverRatio(new Dimension(_imIcon.getIconWidth(), _imIcon.getIconHeight()), _t);
+        Image _nim = _im.getScaledInstance(_nt.width, _nt.height, java.awt.Image.SCALE_SMOOTH);
+        return new JLabel(new ImageIcon(_nim));
     }
 
-    private JTextArea creerContenu(String contenu) {
-        JTextArea _e = new JTextArea(contenu);
-        _e.setOpaque(false);
-        _e.setEditable(false);
-        _e.setFont(new Font("Arial", Font.PLAIN, 14));
-        return _e;
-    }
-
-    public JLabel creerImage(String image, int largeur, int hauteur) {
-        ImageIcon imgIcon = new ImageIcon(image);
-        Image img = imgIcon.getImage();
-        Image newimg = img.getScaledInstance(largeur, hauteur, java.awt.Image.SCALE_SMOOTH);
-        imgIcon = new ImageIcon(newimg);
-        return new JLabel(imgIcon);
-    }
-
+    @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
@@ -190,6 +218,21 @@ public class PanelRegles extends JPanel {
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         //Chargement de l"image de fond
         try {
+            BufferedImage img = ImageIO.read(new File(CHEMIN_RESSOURCE + "/artwork/base.png"));
+            Dimension img_dim = new Dimension(img.getWidth(), img.getHeight());
+            Dimension taille_max = new Dimension((int) (taille_fenetre.width * 0.7), (int) (taille_fenetre.height * 0.7));
+            Dimension taille_redimensionnee = conserverRatio(img_dim, taille_max);
+
+            g2d.drawImage(
+                    img,
+                    getWidth() / 2 - ((int) (taille_redimensionnee.getWidth() / 2)),
+                    getHeight() / 2 - ((int) (taille_redimensionnee.getHeight() / 2)),
+                    taille_redimensionnee.width,
+                    taille_redimensionnee.height,
+                    this
+            );
+
+            
             BufferedImage img_colonnes = ImageIO.read(new File(CHEMIN_RESSOURCE + "/artwork/columns.png"));
             g2d.drawImage(
                     img_colonnes,
@@ -203,6 +246,15 @@ public class PanelRegles extends JPanel {
             e.printStackTrace();
             System.out.println("Erreur image de fond: " + e.getMessage());
         }
+    }
+
+    private Dimension conserverRatio(Dimension imageSize, Dimension boundary) {
+        double widthRatio = boundary.getWidth() / imageSize.getWidth();
+        double heightRatio = boundary.getHeight() / imageSize.getHeight();
+        double ratio = Math.min(widthRatio, heightRatio);
+
+        return new Dimension((int) (imageSize.width * ratio),
+                (int) (imageSize.height * ratio));
     }
 
 }
