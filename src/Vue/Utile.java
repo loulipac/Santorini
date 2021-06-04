43006ab4a2513547787714dcbf0baa5e23ef1d6a
Yanis <yanis.miollany@gmail.com>
2021-06-04 16:53:55 +0200

    refactor : Optimisation des images

--- src/Vue/Utile.java
+++ src/Vue/Utile.java
@@ -26,13 +26,18 @@ public class Utile {
         g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         try {
+            GraphicsConfiguration gfx = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration();
+
             BufferedImage img = ImageIO.read(new File(CHEMIN_RESSOURCE + "/artwork/base.png"));
+
+            BufferedImage newImage = Utile.toCompatibleImage(img);
+
             Dimension img_dim = new Dimension(img.getWidth(), img.getHeight());
             Dimension taille_max = new Dimension((int) (taille_fenetre.width * 0.7), (int) (taille_fenetre.height * 0.7));
             Dimension taille_redimensionnee = Utile.conserverRatio(img_dim, taille_max);
 
-            g.drawImage(
-                    img,
+            g2d.drawImage(
+                    newImage,
                     taille_fenetre.width / 2 - ((int) (taille_redimensionnee.getWidth() / 2)),
                     taille_fenetre.height / 2 - ((int) (taille_redimensionnee.getHeight() / 2)),
                     taille_redimensionnee.width,
@@ -40,8 +45,8 @@ public class Utile {
                     o
             );
 
-
             BufferedImage img_colonnes = ImageIO.read(new File(CHEMIN_RESSOURCE + "/artwork/columns.png"));
+            img_colonnes = Utile.toCompatibleImage(img_colonnes);
             g.drawImage(
                     img_colonnes,
                     0,
@@ -56,6 +61,34 @@ public class Utile {
         }
     }
 
+    private static BufferedImage toCompatibleImage(BufferedImage image) {
+        // obtain the current system graphical settings
+        GraphicsConfiguration gfxConfig = GraphicsEnvironment.
+                getLocalGraphicsEnvironment().getDefaultScreenDevice().
+                getDefaultConfiguration();
+
+        /*
+         * if image is already compatible and optimized for current system
+         * settings, simply return it
+         */
+        if (image.getColorModel().equals(gfxConfig.getColorModel()))
+            return image;
+
+        // image is not optimized, so create a new image that is
+        BufferedImage newImage = gfxConfig.createCompatibleImage(
+                image.getWidth(), image.getHeight(), image.getTransparency());
+
+        // get the graphics context of the new image to draw the old image on
+        Graphics2D g2d = newImage.createGraphics();
+
+        // actually draw the image and dispose of context no longer needed
+        g2d.drawImage(image, 0, 0, null);
+        g2d.dispose();
+
+        // return the new optimized image
+        return newImage;
+    }
+
     public static void dessinePanelBackground(Graphics g, Dimension taille_fenetre, ImageObserver o) {
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
@@ -64,6 +97,7 @@ public class Utile {
 
         try {
             BufferedImage bg_panel = ImageIO.read(new File(CHEMIN_RESSOURCE + "/artwork/bg_regles.png"));
+            bg_panel = Utile.toCompatibleImage(bg_panel);
             g2d.drawImage(
                     bg_panel,
                     0,
