ef4eb382f919309d0cb791256d9fba20e9f63314
Yanis <yanis.miollany@gmail.com>
2021-06-05 00:51:15 +0200

    refactor : Suppression de lignes inutiles dans Santorini.java

--- src/Santorini.java
+++ src/Santorini.java
@@ -8,20 +8,9 @@ public class Santorini implements Runnable {
 
     public void run() {
         fenetre = new Fenetre();
-        //fenetreMenu.getbPlay().addActionListener(this);
     }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Santorini());
     }
-
-    /* @Override
-    public void actionPerformed(ActionEvent e) {
-        if (fenetreMenu.getPlayerVsPlayer().isSelected()) {
-            // A remplir :
-            // Lancer une FenetreJeu à partir de FenetreMenu
-            System.out.println("A FAIRE : Lancer une FenetreJeu");
-        }
-    } */
-
 }
