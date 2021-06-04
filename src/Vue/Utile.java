27bb11aa53d626add3642206b4470996ea770177
Yanis <yanis.miollany@gmail.com>
2021-06-05 00:47:16 +0200

    docs : Fonctions d'Utile

--- src/Vue/Utile.java
+++ src/Vue/Utile.java
@@ -6,6 +6,8 @@ import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import static Modele.Constante.CHEMIN_RESSOURCE;
 
@@ -14,16 +16,28 @@ public class Utile {
     private static BufferedImage column;
     private static BufferedImage parchemin;
 
-    public static Dimension conserverRatio(Dimension imageSize, Dimension boundary) {
-        double widthRatio = boundary.getWidth() / imageSize.getWidth();
-        double heightRatio = boundary.getHeight() / imageSize.getHeight();
-        double ratio = Math.min(widthRatio, heightRatio);
+    /**
+     * Permet de redimensionner une taille d'image selon une taille max tout en gardant le bon ratio de l'image.
+     *
+     * @param tailleImage taille de l'image
+     * @param tailleMax   taille max atteignable
+     * @return
+     */
+    public static Dimension conserverRatio(Dimension tailleImage, Dimension tailleMax) {
+        double largeurRatio = tailleMax.getWidth() / tailleImage.getWidth();
+        double hauteurRatio = tailleMax.getHeight() / tailleImage.getHeight();
+        double ratio = Math.min(largeurRatio, hauteurRatio);
 
-        return new Dimension((int) (imageSize.width * ratio),
-                (int) (imageSize.height * ratio));
+        return new Dimension((int) (tailleImage.width * ratio),
+                (int) (tailleImage.height * ratio));
     }
 
-    public static void dessineBackground(Graphics g, Dimension taille_fenetre, ImageObserver o) {
+    /**
+     * Dessine le dessin d'arrière plan
+     *
+     * @param tailleFenetre taille de la fenetre
+     */
+    public static void dessineBackground(Graphics g, Dimension tailleFenetre, ImageObserver o) {
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
         g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
@@ -32,13 +46,13 @@ public class Utile {
             if(bg == null) bg = ImageIO.read(new File(CHEMIN_RESSOURCE + "/artwork/base.png"));
 
             Dimension img_dim = new Dimension(bg.getWidth(), bg.getHeight());
-            Dimension taille_max = new Dimension((int) (taille_fenetre.width * 0.7), (int) (taille_fenetre.height * 0.7));
+            Dimension taille_max = new Dimension((int) (tailleFenetre.width * 0.7), (int) (tailleFenetre.height * 0.7));
             Dimension taille_redimensionnee = Utile.conserverRatio(img_dim, taille_max);
 
             g2d.drawImage(
                     bg,
-                    taille_fenetre.width / 2 - ((int) (taille_redimensionnee.getWidth() / 2)),
-                    taille_fenetre.height / 2 - ((int) (taille_redimensionnee.getHeight() / 2)),
+                    tailleFenetre.width / 2 - ((int) (taille_redimensionnee.getWidth() / 2)),
+                    tailleFenetre.height / 2 - ((int) (taille_redimensionnee.getHeight() / 2)),
                     taille_redimensionnee.width,
                     taille_redimensionnee.height,
                     o
@@ -50,8 +64,8 @@ public class Utile {
                     column,
                     0,
                     0,
-                    taille_fenetre.width,
-                    taille_fenetre.height,
+                    tailleFenetre.width,
+                    tailleFenetre.height,
                     o
             );
         } catch (Exception e) {
@@ -60,34 +74,37 @@ public class Utile {
         }
     }
 
+    /**
+     * Adapte une image par rapport à l'environnement graphique.
+     *
+     * @param image l'image à adapter
+     * @return l'image adapter
+     */
     private static BufferedImage toCompatibleImage(BufferedImage image) {
-        // obtain the current system graphical settings
         GraphicsConfiguration gfxConfig = GraphicsEnvironment.
                 getLocalGraphicsEnvironment().getDefaultScreenDevice().
                 getDefaultConfiguration();
 
-        /*
-         * if image is already compatible and optimized for current system
-         * settings, simply return it
-         */
+
         if (image.getColorModel().equals(gfxConfig.getColorModel()))
             return image;
 
-        // image is not optimized, so create a new image that is
         BufferedImage newImage = gfxConfig.createCompatibleImage(
                 image.getWidth(), image.getHeight(), image.getTransparency());
 
-        // get the graphics context of the new image to draw the old image on
         Graphics2D g2d = newImage.createGraphics();
 
-        // actually draw the image and dispose of context no longer needed
         g2d.drawImage(image, 0, 0, null);
         g2d.dispose();
 
-        // return the new optimized image
         return newImage;
     }
 
+    /**
+     * Dessine le background style papyrus sur une JPanel.
+     *
+     * @param taille_fenetre taille du dessin background
+     */
     public static void dessinePanelBackground(Graphics g, Dimension taille_fenetre, ImageObserver o) {
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
@@ -114,14 +131,15 @@ public class Utile {
     /**
      * Change une image en mémoire depuis son nom.
      *
-     * @param _name
+     * @param _name nom de l'image (url entière depuis la racine)
      * @return l'image chargé.
      */
     public static Image readImage(String _name) {
         try {
             return ImageIO.read(new FileInputStream(_name));
-        } catch (Exception e) {
-            System.err.println(e);
+        } catch (IOException e) {
+            System.err.println("Erreur : ouverture du fichier : " + _name);
+            e.printStackTrace();
         }
         return null;
     }
