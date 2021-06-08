04d83c3f4763ec13fb360aff7b93c615fa5bdef9
Yanis <yanis.miollany@gmail.com>
2021-06-08 11:28:30 +0200

    docs : Observateur faites

--- src/Patterns/Observateur.java
+++ src/Patterns/Observateur.java
@@ -1,6 +1,11 @@
 package Patterns;
 
+/**
+ * Interface pour mettre à jour différentes vues depuis le modèle
+ */
 public interface Observateur {
-
+    /**
+     * L'appel de cette fonction met à jour les vues qui implémente l'interface lorsqu'elle est appellée
+     */
     void miseAjour();
 }
