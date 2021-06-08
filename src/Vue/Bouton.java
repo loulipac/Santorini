972dfb3549f03e519748bbfed3c6c6f6178e9108
Yanis <yanis.miollany@gmail.com>
2021-06-08 12:14:40 +0200

    refactor, docs : Légère modification de bouton et documentation de ce dernier

--- src/Vue/Bouton.java
+++ src/Vue/Bouton.java
@@ -5,23 +5,19 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class Bouton extends JButton {
-    int largeur, hauteur;
-    String image, imageHover;
+    private int largeur;
+    private int hauteur;
+    private String image;
+    private String imageHover;
 
-    public Bouton(String image, String imageHover, int largeur, int hauteur, ActionListener action) {
-        this(image, imageHover, largeur, hauteur);
-        addActionListener(action);
-    }
-
-    public Bouton(String image, String imageHover, Dimension size) {
-        this(image, imageHover, size.width, size.height);
-    }
-
-    public Bouton(String image, String imageHover, Dimension size, ActionListener action) {
-        this(image, imageHover, size.width, size.height);
-        addActionListener(action);
-    }
-
+    /**
+     * Constructeur principal de bouton dessinant l'image sur sur le bouton et définissant la taille de ce dernier
+     *
+     * @param image      nom de l'image du bouton
+     * @param imageHover nom de l'image du bouton survolé
+     * @param largeur    largeur du bouton
+     * @param hauteur    hauteur du bouton
+     */
     public Bouton(String image, String imageHover, int largeur, int hauteur) {
         this.largeur = largeur;
         this.hauteur = hauteur;
@@ -46,6 +42,38 @@ public class Bouton extends JButton {
         setCursor(new Cursor(Cursor.HAND_CURSOR));
     }
 
+    /**
+     * Constructeur prennant une action à faire quand on clique sur le bouton.
+     */
+    public Bouton(String image, String imageHover, int largeur, int hauteur, ActionListener action) {
+        this(image, imageHover, largeur, hauteur);
+        addActionListener(action);
+    }
+
+    /**
+     * Constructeur prennant une dimension (width, height) pour la taille du bouton.
+     */
+    public Bouton(String image, String imageHover, Dimension size) {
+        this(image, imageHover, size.width, size.height);
+    }
+
+    /**
+     * Constructeur prennant une dimension (width, height) pour la taille du bouton
+     * et une action à faire pour le clique sur le bouton.
+     */
+    public Bouton(String image, String imageHover, Dimension size, ActionListener action) {
+        this(image, imageHover, size.width, size.height);
+        addActionListener(action);
+    }
+
+    /**
+     * Redimensionne l'image en gardant le bon aspect de l'image.
+     *
+     * @param nomImage nom de l'image à redimensionner
+     * @param largeur  largeur de la nouvelle image
+     * @param hauteur  hauteur de la nouvelle image
+     * @return l'image redimensionnée
+     */
     private ImageIcon scaleImage(String nomImage, int largeur, int hauteur) {
         ImageIcon imgIc = new ImageIcon(nomImage);
         Image img = imgIc.getImage();
@@ -53,18 +81,31 @@ public class Bouton extends JButton {
         return new ImageIcon(img);
     }
 
+    /**
+     * Change les images du bouton si elles sont différentes indépendamment.
+     *
+     * @param image      nouvelle image du bouton
+     * @param imageHover nouvelle image du bouton survolé
+     */
     public void changeImage(String image, String imageHover) {
-        if(image.equals(this.image) && imageHover.equals(this.imageHover)) {
-            return;
+        if (!image.equals(this.image)) {
+            this.image = image;
+            ImageIcon iconButton = scaleImage(image, hauteur, largeur);
+            setIcon(iconButton);
         }
-        this.image = image;
-        this.imageHover = imageHover;
-        ImageIcon iconButton = scaleImage(image, hauteur, largeur);
-        setIcon(iconButton);
-        ImageIcon iconButtonHover = scaleImage(imageHover, hauteur, largeur);
-        setRolloverIcon(iconButtonHover);
+        if (!imageHover.equals(this.imageHover)) {
+            this.imageHover = imageHover;
+            ImageIcon iconButtonHover = scaleImage(imageHover, hauteur, largeur);
+            setRolloverIcon(iconButtonHover);
+        }
     }
 
+    /**
+     * Change la taille d'un bouton.
+     *
+     * @param width  la nouvelle largeur
+     * @param height la nouvelle hauteur
+     */
     public void setTaille(int width, int height) {
         hauteur = height;
         largeur = width;
