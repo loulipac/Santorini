1c1419e4e7af18c868f21404a8e73ead21e078bd
Matteo <matteo.doyen@etu.univ-grenoble-alpes.fr>
2021-06-09 03:45:20 +0200

    Refactor(IHM) : Création d'une classe abstraite qui comprend les attributs partagés par PanelTutoriel et PanelPlateau

---
+++ src/Vue/PanelPartie/PanelPartie.java
@@ -0,0 +1,53 @@
+package Vue.PanelPartie;
+
+import Modele.Jeu;
+import Patterns.Observateur;
+import Vue.Panels;
+
+import javax.swing.*;
+import java.awt.*;
+
+public abstract class PanelPartie extends Panels implements Observateur {
+
+    protected JLabel jt;
+    protected Dimension taille_fenetre;
+    protected Font lilyScriptOne;
+    protected Jeu jeu;
+
+    public JLabel getJt() {
+        return jt;
+    }
+
+    public PanelPartie(Dimension _taille_fenetre, Font _lilyScriptOne){
+        taille_fenetre = _taille_fenetre;
+        lilyScriptOne = _lilyScriptOne;
+    }
+
+    public void setJt(JLabel jt) {
+        this.jt = jt;
+    }
+
+    public void setTaille_fenetre(Dimension taille_fenetre) {
+        this.taille_fenetre = taille_fenetre;
+    }
+
+    public void setLilyScriptOne(Font lilyScriptOne) {
+        this.lilyScriptOne = lilyScriptOne;
+    }
+
+    public void setJeu(Jeu jeu) {
+        this.jeu = jeu;
+    }
+
+    public Dimension getTailleFenetre() {
+        return taille_fenetre;
+    }
+
+    public Font getLilyScriptOne() {
+        return lilyScriptOne;
+    }
+
+    public Jeu getJeu() {
+        return jeu;
+    }
+}
